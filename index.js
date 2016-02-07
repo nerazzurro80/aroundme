2547084f1381fc11331c3920df08bf7779649d6f
Salvatore Belcastro <salvatorebelcastro@Mac-60f81dc91f8e.local>
2016-02-07 08:40:20 +0000

    index changes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-app.set('port', (process.env.PORT || 5000));
+// Load the http module to create an http server.
+var http = require('http');
 
-app.get('/', function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World\n");
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
